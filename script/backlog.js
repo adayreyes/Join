8e210674fc85e7afeecd5ade129547c4d85823e6
adayreyes <adayreyes4a@gmail.com>
2022-03-28 19:16:02 +0200

    backlog update

--- script/backlog.js
+++ script/backlog.js
@@ -1,48 +1,66 @@
-function start(){
-    includeHTML();
-    renderTasks();
-
+function start() {
+  includeHTML();
+  renderTasks();
 }
 
-function renderTasks(){
-    let backlog = document.getElementById("backlog-list");
-    backlog.innerHTML = "";
-    addTaskContainer(backlog);
+function renderTasks() {
+  let backlog = document.getElementById("backlog-list");
+  backlog.innerHTML = "";
+  addTaskContainer(backlog);
 }
 
-function addTaskContainer(backlog){
-    for(let i = 0; i < tasks.length; i++){
-        backlog.innerHTML += taskCardTemplate(i);
-        addAssignedToSection(i);
-    } 
+function addTaskContainer(backlog) {
+  for (let i = 0; i < tasks.length; i++) {
+    backlog.innerHTML += taskCardTemplate(i);
+    /*  addAssignedToSection(i); */
+    addNumberOfAssigned(i);
+  }
 }
 
-function addAssignedToSection(i){
+function addNumberOfAssigned(i) {
+  let count = 0;
+  for (let j = 0; j < tasks[i]["assigned"].length; j++) {
+    let container = document.getElementById(`assigned-quantity(${i})`);
+    count++;
+    if (count > 1) {
+      container.innerHTML = "+ " + count;
+    }
+  }
+}
+
+/* function addAssignedToSection(i){
     for(let j = 0; j < tasks[i]["assigned"].length; j++){
         let container = document.getElementById(`assigned-to-container(${i})`);
         container.innerHTML += taskCardAssignedToTemplate(i,j);
-    }
-}
+    } 
+    let count = 0;
+    let container = document.getElementById(`assigned-to-container(${i})`);
+    container.innerHTML += taskCardAssignedToTemplate(0);
+} */
 
-function taskCardTemplate(i){
-    return `
-    <div class="backlog-task ${tasks[i]["category"].toLowerCase()}">
+function taskCardTemplate(i) {
+  return `
+    <div class="backlog-task priority-${tasks[i]["urgency"].toLowerCase()}">
     <div class="assigned-to" id="assigned-to-container(${i})">
+    <img src="${tasks[i]["assigned"][0]["img"]}" alt="">
+    <div class="name-container">
+        <p>${tasks[i]["assigned"][0]["name"]}</p>
+        <a href="" type="email">${tasks[i]["assigned"][0]["email"]}</a>
+        <span id="assigned-quantity(${i})"></span>
+    </div>
     </div>
    <span class="category">${tasks[i]["category"].toUpperCase()}</span>
    <p class="details">${tasks[i]["description"]}</p>
 </div>
-    `
+`;
 }
 
-
-function taskCardAssignedToTemplate(i,j){
+/* function taskCardAssignedToTemplate(i,j){
     return `
-    <img src="${tasks[i]["assigned"][j]["img"]}" id="user(${j})" alt="">
     <div class="name-container">
-        <p>${tasks[i]["assigned"][j]["name"]}</p>
-        <a href="" type="email">${tasks[i]["assigned"][j]["email"]}</a>
+        <p>${tasks[i]["assigned"][0]["name"]}</p>
+        <a href="" type="email">${tasks[i]["assigned"][0]["email"]}</a>
+        <span id="assigned-quantity(${i})"></span>
     </div>
-    `
-    
-}
+    ` 
+} */
