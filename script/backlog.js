7127c2e536388200f458cc21a71639e6a2e70a61
adayreyes <adayreyes4a@gmail.com>
2022-03-29 09:47:48 +0200

    backlog showTask designed

--- script/backlog.js
+++ script/backlog.js
@@ -40,7 +40,9 @@ function addNumberOfAssigned(i) {
 
 function taskCardTemplate(i) {
   return `
-    <div onclick="showBacklogTask(${i})" class="backlog-task priority-${tasks[i]["urgency"].toLowerCase()}">
+    <div onclick="openBacklogTask(${i})" class="backlog-task priority-${tasks[
+    i
+  ]["urgency"].toLowerCase()}">
     <div class="assigned-to" id="assigned-to-container(${i})">
     <img src="${tasks[i]["assigned"][0]["img"]}" alt="">
     <div class="name-container">
@@ -65,7 +67,66 @@ function taskCardTemplate(i) {
     ` 
 } */
 
-function showBacklogTask(i){
-    let big_container = document.getElementById("big-task-container");
-    big_container.classList.remove("d-none");
+function openBacklogTask(i) {
+  showBigContainer();
+  renderBigTask(i);
+}
+
+function renderBigTask(i) {
+  let sub_container = document.getElementById("big-task-subcontainer");
+  sub_container.innerHTML = "";
+  newBigTaskSection(sub_container, i, "title");
+  newBigTaskSection(sub_container, i, "category");
+  newBigTaskSection(sub_container, i, "urgency");
+  newBigTaskSection(sub_container, i, "description");
+  newBigTaskSection(sub_container, i, "date");
+  setAssignedSection(sub_container, "assigned");
+  newAssignedSection(i);
+}
+
+function setAssignedSection(container, section) {
+  container.innerHTML += `
+  <div>
+  <span class="font-title">ASSIGNED TO</span>
+  <p id="big-task-assigned-people"></p>
+</div>
+  `;
+}
+
+function newAssignedSection(i) {
+  let assigned_container = document.getElementById("big-task-assigned-people");
+  tasks[i]["assigned"].forEach((person) => {
+    assigned_container.innerHTML += `
+    <div class="assigned-to">
+    <img src="${person["img"]}" alt="">
+    <div class="name-container">
+        <p>${person["name"]}</p>
+        <a href="" type="email">${person["email"]}</a>
+    </div>
+    </div>
+    `;
+  });
+}
+
+function newBigTaskSection(container, i, section) {
+  container.innerHTML += `
+  <div>
+    <span class="font-title">${section.toUpperCase()}</span>
+    <p id="big-task-${section}">${tasks[i][section]}</p>
+  </div>
+  `;
+}
+
+function showBigContainer() {
+  let big_container = document.getElementById("big-task-container");
+  let backlog_list = document.getElementById("backlog-list");
+  big_container.classList.remove("d-none");
+  backlog_list.classList.add("d-none");
+}
+
+function closeBigTask() {
+  let big_container = document.getElementById("big-task-container");
+  let backlog_list = document.getElementById("backlog-list");
+  big_container.classList.add("d-none");
+  backlog_list.classList.remove("d-none");
 }
